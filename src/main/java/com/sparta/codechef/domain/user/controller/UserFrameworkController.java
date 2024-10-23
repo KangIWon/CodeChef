c6b6bc04cee34ac47d98166ec40b332f407966b4
MinJun Song <j34679@naver.com>
2024-10-24 04:17:30 +0900

    [update] Userframework CRU 구현

---
+++ src/main/java/com/sparta/codechef/domain/user/controller/UserFrameworkController.java
@@ -0,0 +1,39 @@
+package com.sparta.codechef.domain.user.controller;
+
+
+import com.sparta.codechef.common.ApiResponse;
+import com.sparta.codechef.domain.framework.dto.FrameworkRequest;
+import com.sparta.codechef.domain.user.service.UserFrameworkService;
+import com.sparta.codechef.security.AuthUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserFrameworkController {
+
+    private final UserFrameworkService userFrameworkService;
+
+    @PostMapping("users/frameworks/assign")
+    public ApiResponse createUserFramework(@AuthenticationPrincipal AuthUser authUser, List<FrameworkRequest> frameworkRequestList)
+    {
+        return ApiResponse.createSuccess(HttpStatus.OK.value(), "유저 프레임워크가 설정되었습니다.", userFrameworkService.createUserFramework(authUser, frameworkRequestList));
+    }
+    @GetMapping("users/frameworks")
+    public ApiResponse getUserFramework(@AuthenticationPrincipal AuthUser authUser)
+    {
+        return ApiResponse.createSuccess(HttpStatus.OK.value(), "유저 프레임워크 조회에 성공했습니다.", userFrameworkService.getUserFrameworks(authUser));
+    }
+    @PatchMapping("users/frameworks")
+    public ApiResponse updateUserFramework(@AuthenticationPrincipal AuthUser authUser, List<FrameworkRequest> frameworkRequestList)
+    {
+        return ApiResponse.createSuccess(HttpStatus.OK.value(), "유저 프레임워크 수정에 성공했습니다.", userFrameworkService.updateUserFramework(authUser));
+    }
+
+
+}
--- src/main/java/com/sparta/codechef/domain/user/repository/UserFrameworkRepository.java
+++ src/main/java/com/sparta/codechef/domain/user/repository/UserFrameworkRepository.java
@@ -4,7 +4,11 @@ import com.sparta.codechef.domain.user.entity.UserFramework;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface UserFrameworkRepository extends JpaRepository<UserFramework, Long> {
+
+    List<UserFramework> findAllByUserId(Long userId);
 }
