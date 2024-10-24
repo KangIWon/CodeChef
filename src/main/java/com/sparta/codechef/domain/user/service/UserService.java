b25fa4972d0f65884be376979e54666abaaf009d
naminsu <naminsoo1020@naver.com>
2024-10-25 00:28:03 +0900

    [fix] 출석여부 자동 업데이트 기능 조건 추가, 미 출첵시 포인트 감소 기능 수정

--- src/main/java/com/sparta/codechef/domain/user/service/UserService.java
+++ src/main/java/com/sparta/codechef/domain/user/service/UserService.java
@@ -8,8 +8,6 @@ import com.sparta.codechef.domain.user.entity.User;
 import com.sparta.codechef.domain.user.repository.UserRepository;
 import com.sparta.codechef.security.AuthUser;
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -22,40 +20,20 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    // 자동으로 IsAttend false로 바꾸는 것
+
     @Scheduled(cron = "0 0 0 * * *")
     @Transactional
     public void resetAllIsAttend() {
         userRepository.resetIsAttend();
     }
 
-    // 자동으로 포인트 감소하는 것
+    // 일주일 미출첵시, 자동으로 포인트 감소
     @Scheduled(cron = "0 0 0 * * *")
     @Transactional
     public void decreasePointsAutomatically() {
-        List<User> users = userRepository.findAll();
-        for (User user : users) {
-            if (shouldDecreasePoints(user)) {
-                decreasePoints(user);
-            }
-        }
-    }
-
-    // 마지막 출석일로부터 7일 지났는지 확인하는 메서드
-    private boolean shouldDecreasePoints(User user) {
-        LocalDate lastAttendDate = user.getLastAttendDate();
-        LocalDate currentDate = LocalDate.now();
-        return lastAttendDate != null && ChronoUnit.DAYS.between(lastAttendDate, currentDate) >= 7;
-    }
-
-    // 포인트 10% 차감 및 마지막 차감일 업데이트 메서드
-    private void decreasePoints(User user) {
-        Integer currentPoint = user.getPoint();
-        Integer decreasedPoint = currentPoint - (currentPoint / 10);
-
-        user.updatePoint(decreasedPoint);
-        user.updateLastAttendDate();
-        userRepository.save(user);
+        LocalDate sevenDaysAgo = LocalDate.now().minusDays(7);
+        LocalDate today = LocalDate.now();
+        userRepository.decreaseAutomatically(sevenDaysAgo, today);
     }
 
     @Transactional
