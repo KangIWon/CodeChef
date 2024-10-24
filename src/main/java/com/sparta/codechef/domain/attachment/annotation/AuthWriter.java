4a6250a20dce7b0d2fe3187bc064f3e19dfa791d
MinjuKang <minjukang727@gmail.com>
2024-10-24 16:14:28 +0900

    update : 게시글 작성자 권한 체크용 어노테이션 추가 @AuthWriter

---
+++ src/main/java/com/sparta/codechef/domain/attachment/annotation/AuthWriter.java
@@ -0,0 +1,14 @@
+package com.sparta.codechef.domain.attachment.annotation;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("@boardService.isWriter(authentication.principal.getUserId(), #boardId)")
+public @interface AuthWriter {
+}
