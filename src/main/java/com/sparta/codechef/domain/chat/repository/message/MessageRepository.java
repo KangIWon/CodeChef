cca6355cb7408293f10bb70b3267bd9256e9cdf9
MinjuKang <minjukang727@gmail.com>
2024-10-25 18:37:54 +0900

    update: 메세지 전체 조회 쿼리메서드 추가

--- src/main/java/com/sparta/codechef/domain/chat/repository/message/MessageRepository.java
+++ src/main/java/com/sparta/codechef/domain/chat/repository/message/MessageRepository.java
@@ -2,8 +2,14 @@ package com.sparta.codechef.domain.chat.repository.message;
 
 import com.sparta.codechef.domain.chat.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long>, MessageQueryDslRepository {
+
+    @Query("SELECT m FROM Message m WHERE m.chatRoom.id = :chatRoomId AND m.isDeleted = false")
+    List<Message> findAllMessagesByChatRoomId(Long chatRoomId);
 }
