884435568201abb71719a376d1a381bb2273c73c
MinjuKang <minjukang727@gmail.com>
2024-10-29 10:34:48 +0900

    update: MySQL 기반 채팅 구현 v1 패키지로 이동, exist 쿼리 메서드 삭제
    - existsByIdAndUserId() : exist 쿼리에서 에러가 나서 삭제하고 QueryDsl로 작성

--- src/main/java/com/sparta/codechef/domain/chat/v1/repository/chat_room/ChatRoomRepository.java
+++ src/main/java/com/sparta/codechef/domain/chat/v1/repository/chat_room/ChatRoomRepository.java
@@ -1,7 +1,7 @@
-package com.sparta.codechef.domain.chat.repository.chat_room;
+package com.sparta.codechef.domain.chat.v1.repository.chat_room;
 
 
-import com.sparta.codechef.domain.chat.entity.ChatRoom;
+import com.sparta.codechef.domain.chat.v1.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,9 +13,6 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, ChatR
 
     @Query("SELECT c FROM ChatRoom c WHERE c.id = :chatRoomId AND c.user.id = :userId")
     Optional<ChatRoom> findByIdAndUser(Long chatRoomId, Long userId);
-
-    @Query("SELECT exists(SELECT c FROM ChatRoom c WHERE c.id = :chatRoomId AND c.user.id = :userId AND c.isDeleted = false)")
-    boolean existsByIdAndUserId(Long chatRoomId, Long userId);
 
     @Query("SELECT c FROM ChatRoom c WHERE c.id = :chatRoomId AND c.isDeleted = false")
     Optional<ChatRoom> findExistChatRoomById(Long chatRoomId);
