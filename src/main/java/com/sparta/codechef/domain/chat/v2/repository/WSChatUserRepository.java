15e8610fcc1083f9a34b8950b9e26d35ff48f8d6
MinjuKang <minjukang727@gmail.com>
2024-10-29 11:30:38 +0900

    update: Redis CRUD를 위한 레포지토리 추가

---
+++ src/main/java/com/sparta/codechef/domain/chat/v2/repository/WSChatUserRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.codechef.domain.chat.v2.repository;
+
+import com.sparta.codechef.domain.chat.v2.entity.WSChatUser;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WSChatUserRepository extends CrudRepository<WSChatUser,Long>, WSChatRepository {
+}
