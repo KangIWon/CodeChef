2e798ea70e6af487a49cf698e2e767a5db82e381
naminsu <naminsoo1020@naver.com>
2024-11-04 19:41:10 +0900

    [update] CloudFront 기능 구현 및 환경 설정

--- src/main/java/com/sparta/codechef/config/RedisConfig.java
+++ src/main/java/com/sparta/codechef/config/RedisConfig.java
@@ -50,7 +50,18 @@ public class RedisConfig {
         return redisTemplate;
     }
 
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSentinelServers()
+                .setMasterName("mymaster")
+                .setCheckSentinelsList(false) // Sentinel 최소 개수 체크 비활성화
+                .addSentinelAddress("redis://" + localhost + ":26379",
+                        "redis://" + localhost + ":26380",
+                        "redis://" + localhost + ":26381");
 
+        return Redisson.create(config);
+    }
 
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
@@ -68,18 +79,13 @@ public class RedisConfig {
                 .transactionAware() // 트랜잭션 지원
                 .build();
     }
-
-    // Redisson 분산 락을 위한 클라이언트 Bean 설정
-    @Bean
-    public RedissonClient redissonClient() {
-        Config config = new Config();
-        config.useSentinelServers()
-                .setMasterName("mymaster")
-                .setCheckSentinelsList(false) // Sentinel 최소 개수 체크 비활성화
-                .addSentinelAddress("redis://" + localhost + ":26379",
-                        "redis://" + localhost + ":26380",
-                        "redis://" + localhost + ":26381");
-
-        return Redisson.create(config);
-    }
 }
+
+
+
+
+
+
+
+
+
