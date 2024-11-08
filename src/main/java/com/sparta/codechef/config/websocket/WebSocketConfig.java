18bd549d6d8fcc07076f559eb0bce0c155bebe28
MinjuKang <minjukang727@gmail.com>
2024-11-08 22:03:39 +0900

    update : 웹 소켓 설정을 domain 밖의 config 파일로 이동

---
+++ src/main/java/com/sparta/codechef/config/websocket/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package com.sparta.codechef.config.websocket;
+
+import com.sparta.codechef.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic", "/queue");
+        registry.setApplicationDestinationPrefixes("/app"); // 송신 경로
+        registry.setPreservePublishOrder(true);
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-chat")  // STOMP 엔드포인트
+                .addInterceptors(new ChatHandshakeInterceptor(jwtUtil))
+                .setAllowedOriginPatterns("*");
+    }
+}
