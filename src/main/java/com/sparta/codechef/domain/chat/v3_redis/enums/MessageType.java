7ae325062237b7be3197043fe30fef51386a804d
MinjuKang <minjukang727@gmail.com>
2024-11-08 22:48:26 +0900

    update :  v3용 MessageType 추가
    - CREATE : 채팅방 생성
    - IN : 유저 입장
    - OUT : 유저 퇴장
    - NEW_HOST : 방장 변경
    - SEND : 메세지 전송
    
    info : 기본 메세지 내용
    MessageType.of() : String type으로 MessageType 반환
    toString() : 기본 메세지 내용 반환
    toValue() : Redis 데이터 저장을 위해 사용 enum의 name반환

---
+++ src/main/java/com/sparta/codechef/domain/chat/v3_redis/enums/MessageType.java
@@ -0,0 +1,40 @@
+package com.sparta.codechef.domain.chat.v3_redis.enums;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.sparta.codechef.common.ErrorStatus;
+import com.sparta.codechef.common.exception.ApiException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+@Getter
+@RequiredArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.STRING)
+public enum MessageType {
+    CREATE("채팅방이 개설되었습니다.\n[ ${EMAIL} ] 님이 방장입니다."),
+    IN("[ ${EMAIL} ] 님이 채팅방에 입장하셨습니다."),
+    OUT("[ ${EMAIL} ] 님이 채팅방에서 퇴장하셨습니다."),
+    NEW_HOST("[ ${EMAIL} ] 님이 새로운 방장이 되셨습니다."),
+    SEND("");
+
+
+    private final String info;
+
+    public static MessageType of(String type) {
+        return Arrays.stream(MessageType.values())
+                .filter(messageType -> messageType.name().equalsIgnoreCase(type))
+                .findFirst()
+                .orElseThrow(() -> new ApiException(ErrorStatus.INVALID_MESSAGE_TYPE));
+    }
+
+    public String toString(String email) {
+        return this.info.replace("${EMAIL}", email);
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
+}
