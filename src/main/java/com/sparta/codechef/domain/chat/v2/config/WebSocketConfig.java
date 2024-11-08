05e6fe5c0d61433b5291d30f78a4eab8ce4aab25
MinjuKang <minjukang727@gmail.com>
2024-11-08 22:03:07 +0900

    update : v2. 웹소켓 설정 주석처리

--- src/main/java/com/sparta/codechef/domain/chat/v2/config/WebSocketConfig.java
+++ src/main/java/com/sparta/codechef/domain/chat/v2/config/WebSocketConfig.java
@@ -1,31 +1,31 @@
-package com.sparta.codechef.domain.chat.v2.config;
-
-import com.sparta.codechef.security.JwtUtil;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-@Configuration
-@RequiredArgsConstructor
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    private final JwtUtil jwtUtil;
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic"); // 구독 경로
-        registry.setApplicationDestinationPrefixes("/app"); // 송신 경로
-        registry.setPreservePublishOrder(true);  // 메시지 게시 순서를 보장
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-chat")  // STOMP 엔드포인트
-                .addInterceptors(new ChatHandshakeInterceptor(jwtUtil))
-                .setAllowedOriginPatterns("*");
-    }
-}
+//package com.sparta.codechef.domain.chat.v2.config;
+//
+//import com.sparta.codechef.security.JwtUtil;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//@EnableWebSocketMessageBroker
+//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//
+//    private final JwtUtil jwtUtil;
+//
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        registry.enableSimpleBroker("/topic"); // SimpleBroker구독 경로
+//        registry.setApplicationDestinationPrefixes("/app"); // 송신 경로
+//        registry.setPreservePublishOrder(true);  // 메시지 게시 순서를 보장
+//    }
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/ws-chat")  // STOMP 엔드포인트
+//                .addInterceptors(new ChatHandshakeInterceptor(jwtUtil))
+//                .setAllowedOriginPatterns("*");
+//    }
+//}
